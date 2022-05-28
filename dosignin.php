20939d6d962eaa5f430e5163945cc7d2bbbbb3a7
dsprz <84564775+dsprz@users.noreply.github.com>
2022-05-28 02:50:14 +0200

    Delete dosignin.php

--- dosignin.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include("Authentificateur.php");
-    $login = $_POST["login"];
-    $password = $_POST["password"];
-    $check = new Authentificateur($login, $password);
-    $check->signIn();
-?>
