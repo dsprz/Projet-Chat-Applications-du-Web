9de27a102becc6958493ec6d89bc9ce3fdad220e
dsprz <84564775+dsprz@users.noreply.github.com>
2022-05-28 02:50:01 +0200

    Delete addUser.php

--- addUser.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include("Authentificateur.php");
-    $login = $_POST["login"];
-    $password = $_POST["password"];
-    $confirmedPassword = $_POST["confirmedPassword"];
-    $auth = new Authentificateur($login, $password, $confirmedPassword);
-    $auth->addUser();
-?>
